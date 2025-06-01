018c4d57ab613787e3853372cd93577dd91d4e4e
dolichotis <60782357+dolichotis@users.noreply.github.com>
2025-06-01 17:19:44 +0300

    main style

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -5,6 +5,7 @@ import './main.css';
 function Main() {
   return (
     <div className="main" id="mainID">
+      <div className="overlay"></div>
       <ul className="text-container">
         <li className="liMain">Более 10 лет опыта реконструкций строений из древесины</li>
         <li className="liMain">Гарантии на сроки выполнения работ, чистоту и порядок на объекте</li>
