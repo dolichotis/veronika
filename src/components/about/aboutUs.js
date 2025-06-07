d17d58fdfa8be716778c8a877019f117b2ad8ce9
dolichotis <60782357+dolichotis@users.noreply.github.com>
2025-06-07 20:30:34 +0300

    main style img

--- src/components/about/aboutUs.js
+++ src/components/about/aboutUs.js
@@ -16,31 +16,33 @@ const contentStyle = {
 };
 
 const AboutUs = () => (
-  <ConfigProvider
-    theme={{
-      token: {
-        fontSize: 16
-      },
-    }}
-  >
-    <Carousel autoplay>
-      <div>
-        <h3 style={contentStyle}>Накоплен большой опыт в реставрации деревянных домов. Мастера работают более 10 лет.</h3>
-      </div>
-      <div>
-        <h3 style={contentStyle}>Работаем только на качественном и проверенном оборудовании</h3>
-      </div>
-      <div>
-        <h3 style={contentStyle}>Сдаем объект всегда в обговоренный срок. Быстро и качественно выполняем все работы.</h3>
-      </div>
-      <div>
-        <h3 style={contentStyle}>Сэкономим Ваш бюджет. Выполняем только те работы, которые действительно нужны.</h3>
-      </div>
-      <div>
-        <h3 style={contentStyle}>Бесплатная уборка после выполнения работ.</h3>
-      </div>
-    </Carousel>
-  </ConfigProvider>
+    <ConfigProvider
+      theme={{
+        token: {
+          fontSize: 16
+        },
+      }}
+    >
+      <Carousel autoplay>
+        <div>
+          <h3 style={contentStyle}>Накоплен большой опыт в реставрации деревянных домов. Мастера
+            работают более 10 лет.</h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>Работаем только на качественном и проверенном оборудовании</h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>Сдаем объект всегда в обговоренный срок. Быстро и качественно выполняем все работы.</h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>Сэкономим Ваш бюджет. Выполняем только те работы, которые действительно нужны.</h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>Бесплатная уборка после выполнения работ.</h3>
+        </div>
+      </Carousel>
+    </ConfigProvider>
+
 );
 export default AboutUs;
 
--- src/components/main/main.js
+++ src/components/main/main.js
@@ -7,9 +7,9 @@ function Main() {
     <div className="main" id="mainID">
       <div className="overlay"></div>
       <ul className="text-container">
+        <li className="liMain">Работаем по Московской области до 300км от Москвы</li>
         <li className="liMain">Более 10 лет опыта реконструкций строений из древесины</li>
         <li className="liMain">Гарантии на сроки выполнения работ, чистоту и порядок на объекте</li>
-        <li className="liMain">Работаем по Московской области до 300км от Москвы</li>
       </ul>
     </div>
   )
